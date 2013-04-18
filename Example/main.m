5b30d791f3c13ef914b5f000fb899dba0b51cee3
Jeremy Templier <templier.jeremy@gmail.com>
2013-04-17 19:44:41 -0700

    add example project target

---
+++ Example/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Example
+//
+//  Created by Jeremy Templier on 4/17/13.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
